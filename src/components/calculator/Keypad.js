a114e10a90198667c616fd8b3e407ea09c35af23
Alishka <141027048+alishka810@users.noreply.github.com>
2023-10-29 21:01:26 +0300

    Update Keypad.js

--- src/components/calculator/Keypad.js
+++ src/components/calculator/Keypad.js
@@ -9,25 +9,25 @@ const Keypad = (props) => {
           props.updateCalc("7");
         }}
       >
-        45
+        99
       </button>
       <button className="key" value="8" onClick={() => props.updateCalc("8")}>
-        62
+        99
       </button>
       <button className="key" value="9" onClick={() => props.updateCalc("9")}>
-        44
+        99
       </button>
       <button className="del" id="del" onClick={props.del}>
         DEL
       </button>
       <button className="key" value="4" onClick={() => props.updateCalc("4")}>
-        71
+        99
       </button>
       <button className="key" value="5" onClick={() => props.updateCalc("5")}>
-        88
+        99
       </button>
       <button className="key" value="6" onClick={() => props.updateCalc("6")}>
-        11
+        99
       </button>
       <button
         className="operator"
@@ -37,13 +37,13 @@ const Keypad = (props) => {
         +
       </button>
       <button className="key" value="1" onClick={() => props.updateCalc("1")}>
-        34
+        99
       </button>
       <button className="key" value="2" onClick={() => props.updateCalc("2")}>
-        71
+        99
       </button>
       <button className="key" value="3" onClick={() => props.updateCalc("3")}>
-        18
+        99
       </button>
       <button
         className="operator"
