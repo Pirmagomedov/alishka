6363c8080b160057f2445e1ac6b50271e502ef16
alishka810 <alimirza@wingform.com>
2023-11-02 21:12:57 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,111 @@
-import Home from "./pages/Home";
-import Support from "./pages/Support";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import WeatherApp from "./pages/Weather";
-import WeatherMain from "./pages/WeatherMain";
-import NotFound from "./pages/404";
-import ForecastWeather from "./pages/ForecastWeather";
-import Settings from "./pages/Settings";
-import { db } from "./backend/app_backend";
-import "./autoload";
+// Very first React project :D
 
-function App() {
-  let homePageSeen = db.get("HOME_PAGE_SEEN");
-  let DEFAULT_ROUTE_PAGE;
-  homePageSeen
-    ? (DEFAULT_ROUTE_PAGE = <WeatherApp />)
-    : (DEFAULT_ROUTE_PAGE = <Home />);
+import "./App.css";
+import { useState } from "react";
+// useState is a React hook that takes a state's initial value and returns an array with 2 values,
+// i.e., a getter and setter function
+
+import { MdAdd } from "react-icons/md";
+import { SlTrash } from "react-icons/sl";
+import { VscGithub } from "react-icons/vsc";
+import {SiLinkedin } from "react-icons/si";
+
+const TodoInput = ({ todo, setTodo, addTodo, setTodos, todos }) => {
+  return (
+    <div className="input-wrapper">
+      <input
+        type="text"
+        id="todo-input"
+        name="todo"
+        value={todo}
+        placeholder="Let's get workin'..."
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+        onKeyUp={(e) => {
+          if(e.key==='Enter') {
+            e.preventDefault();
+            setTodo(e.target.value);
+            setTodos([...todos, todo]);
+            setTodo('');
+          }
+        }}
+      ></input>
+      <button className="add-button" id="urmom" onClick={addTodo}>
+        <MdAdd size={21}/>
+      </button>
+    </div>
+  );
+};
+
+const TodoList = ({ todoList, removeTodo }) => {
+  return (
+    <div className="input-list">
+      {todoList?.length > 0 ? (
+        <ul className="todo-list">
+          {todoList.map((entry, index) => (
+            <div className="todo">
+              <li key={index}>{entry}</li>
+              <button className="delete-button" onClick={()=>{removeTodo(entry)}}>
+                <SlTrash size={18}/>
+
+              </button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <p>Add some tasks! :)</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <></>
+  );
+};
+
+const App = () => {
+  // Create a todo
+  const [todo, setTodo] = useState("");
+  // Getter function: todo -> displays current state
+  // In this case, initial value is undefined, i.e., ""
+  // Setter function: setTodo -> sets/updates state
+
+  // Add a todo
+  const [todos, setTodos] = useState([]);
+  // Getter function: todos -> empty array (todos are pushed to it)
+  // Setter function: setTodos -> updates todos array using addTodo()
+
+  // Function to add todo to array
+  const addTodo = () => {
+    if (todo !== "") {
+      // Ensures that input isn't empty
+      setTodos([...todos, todo]);
+      setTodo(""); // Clears input after todo is pushed to array
+    }
+  };
+  // todo is pushed to todos (a copy of todos using ... operator)
+
+  const deleteTodo = (task) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== task;
+    });
+    setTodos(newTodos);
+  };
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={DEFAULT_ROUTE_PAGE} />
-        <Route path="support" element={<Support />} />
-        <Route path="weather" element={<WeatherApp />} />
-        <Route path="weathermain" element={<WeatherMain />} />
-        <Route path="forecast" element={<ForecastWeather />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <h1>Make a To-Do List!</h1>
+      <div className="Content">
+        <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} setTodos={setTodos} todos={todos}/>
+        <TodoList todoList={todos} removeTodo={deleteTodo} />
+      </div>
+      <Footer/>
+    </div>
   );
 }
 
