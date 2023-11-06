c77d770da1fcd749f41ed00f274e5cac506518c8
alishka810 <alimirza@wingform.com>
2023-11-06 20:40:32 +0300

    sdf

--- src/App.js
+++ src/App.js
@@ -93,9 +93,7 @@ const App = () => {
     </div>
     <footer>
       <textarea placeholder="Type your message"></textarea>
-      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt="" />
-      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt="" />
-      <a href="#">Send</a>
+      <p>Send</p>
     </footer>
   </main>
 </div>
