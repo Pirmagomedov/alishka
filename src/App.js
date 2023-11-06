a7c743f710ef75b06ff75d9fb84abf890ad3f11c
alishka810 <alimirza@wingform.com>
2023-11-07 00:58:35 +0300

    WebSockets

--- src/App.js
+++ src/App.js
@@ -7,104 +7,36 @@ import { useState } from "react";
 
 const App = () => {
   const [ws, setWs] = useState(null)
+  const [input, setInput] = useState("")
+  const [message, setMessage] = useState("")
 
   useEffect(() => {
     const ws = new WebSocket("wss://www.instagram-vpn.ru/ws/")
     ws.onopen = (obj) => {
       console.log("Open!")
     }
+    ws.onmessage = (obj) => {
+      setMessage(JSON.parse(obj.data).message)
+    }
     setWs(ws)
   }, [])
 
+  const ali = () => {
+    setInput("")
+    ws.send(JSON.stringify({
+      message: input
+    }))
+  }
+
   return (
     <div className="App">
-      <div id="container">
-  <main>
-
-    <div id="aaa">
-    <ul id="chat">
-      <li class="you">
-        <div class="entete">
-          <span class="status green"></span>
-          <h2>Vincent</h2>
-          <h3>10:12AM, Today</h3>
-        </div>
-        <div class="triangle"></div>
-        <div class="message">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-        </div>
-      </li>
-      <li class="me">
-        <div class="entete">
-          <h3>10:12AM, Today</h3>
-          <h2>Vincent</h2>
-          <span class="status blue"></span>
-        </div>
-        <div class="triangle"></div>
-        <div class="message">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-        </div>
-      </li>
-      <li class="me">
-        <div class="entete">
-          <h3>10:12AM, Today</h3>
-          <h2>Vincent</h2>
-          <span class="status blue"></span>
-        </div>
-        <div class="triangle"></div>
-        <div class="message">
-          OK
-        </div>
-      </li>
-      <li class="you">
-        <div class="entete">
-          <span class="status green"></span>
-          <h2>Vincent</h2>
-          <h3>10:12AM, Today</h3>
-        </div>
-        <div class="triangle"></div>
-        <div class="message">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-        </div>
-      </li>
-      <li class="me">
-        <div class="entete">
-          <h3>10:12AM, Today</h3>
-          <h2>Vincent</h2>
-          <span class="status blue"></span>
-        </div>
-        <div class="triangle"></div>
-        <div class="message">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-        </div>
-      </li>
-      <li class="me">
-        <div class="entete">
-          <h3>10:12AM, Today</h3>
-          <h2>Vincent</h2>
-          <span class="status blue"></span>
-        </div>
-        <div class="triangle"></div>
-        <div class="message">
-          OK
-        </div>
-      </li>
-    </ul>
+      <div>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button onClick={ali}>Отправить</button>
+        <div className="message">{message}</div>
+      </div> 
     </div>
-    <footer>
-      <textarea placeholder="Type your message"></textarea>
-      <p>Send</p>
-    </footer>
-  </main>
-</div>
-      {/* <h1>Make a To-Do List!</h1>
-      <div className="Content">
-        <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} setTodos={setTodos} todos={todos}/>
-        <TodoList todoList={todos} removeTodo={deleteTodo} />
-      </div>
-      <Footer/> */}
-    </div>
-  );
+  )
 }
 
 export default App;
