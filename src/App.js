db1419b45460def767b26229f1b1601887c852dc
alishka810 <alimirza@wingform.com>
2023-11-06 20:21:41 +0300

    add chat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Very first React project :D
 
 import "./App.css";
+import { useEffect } from "react";
 import { useState } from "react";
 // useState is a React hook that takes a state's initial value and returns an array with 2 values,
 // i.e., a getter and setter function
@@ -9,8 +10,29 @@ import { MdAdd } from "react-icons/md";
 import { SlTrash } from "react-icons/sl";
 
 const TodoInput = ({ todo, setTodo, addTodo, setTodos, todos }) => {
+  const [ws, setWs] = useState(null)
+
+  useEffect(() => {
+    const ws = new WebSocket("wss://www.instagram-vpn.ru/ws/")
+    ws.onopen = (obj) => {
+      console.log("Open!")
+    }
+    setWs(ws)
+  }, [])
+
+  const ali = () => {
+    ws.send(
+      JSON.stringify(
+        {
+          message: todo
+        }
+      )
+    )
+  }
+
   return (
     <div className="input-wrapper">
+      <button onClick={ali}>WEB</button>
       <input
         type="text"
         id="todo-input"
@@ -97,12 +119,93 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Make a To-Do List!</h1>
+      <div id="container">
+  <main>
+
+    <div id="aaa">
+    <ul id="chat">
+      <li class="you">
+        <div class="entete">
+          <span class="status green"></span>
+          <h2>Vincent</h2>
+          <h3>10:12AM, Today</h3>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+        </div>
+      </li>
+      <li class="me">
+        <div class="entete">
+          <h3>10:12AM, Today</h3>
+          <h2>Vincent</h2>
+          <span class="status blue"></span>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+        </div>
+      </li>
+      <li class="me">
+        <div class="entete">
+          <h3>10:12AM, Today</h3>
+          <h2>Vincent</h2>
+          <span class="status blue"></span>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          OK
+        </div>
+      </li>
+      <li class="you">
+        <div class="entete">
+          <span class="status green"></span>
+          <h2>Vincent</h2>
+          <h3>10:12AM, Today</h3>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+        </div>
+      </li>
+      <li class="me">
+        <div class="entete">
+          <h3>10:12AM, Today</h3>
+          <h2>Vincent</h2>
+          <span class="status blue"></span>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+        </div>
+      </li>
+      <li class="me">
+        <div class="entete">
+          <h3>10:12AM, Today</h3>
+          <h2>Vincent</h2>
+          <span class="status blue"></span>
+        </div>
+        <div class="triangle"></div>
+        <div class="message">
+          OK
+        </div>
+      </li>
+    </ul>
+    </div>
+    <footer>
+      <textarea placeholder="Type your message"></textarea>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt="" />
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt="" />
+      <a href="#">Send</a>
+    </footer>
+  </main>
+</div>
+      {/* <h1>Make a To-Do List!</h1>
       <div className="Content">
         <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} setTodos={setTodos} todos={todos}/>
         <TodoList todoList={todos} removeTodo={deleteTodo} />
       </div>
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 }
