db87b1e294bd61eb11c75f9e1a51c81d123c9a2d
Alishka <141027048+alishka810@users.noreply.github.com>
2023-12-29 16:04:14 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -50,7 +50,7 @@ const Home = () => {
   return (
     <div>
       <input
-        placeholder="Ваше имя..."
+        placeholder="🏐Ваше имя..."
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
