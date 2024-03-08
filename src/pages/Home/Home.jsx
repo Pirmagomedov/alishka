e18221e329e742daf31cf4e1455ff6a4600e2836
Alishka <141027048+alishka810@users.noreply.github.com>
2024-03-09 00:40:18 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -360,7 +360,7 @@ const Home = () => {
             }
             if (a === page)
               return (
-                <div style={{ fontWeight: "bold", color: "green" }}>{page}</div>
+                <div style={{ fontWeight: "bold", color: "green","border": "none !important", "outline": "none !important"}}>{page}</div>
               );
 
             if (b === "jump-next")
