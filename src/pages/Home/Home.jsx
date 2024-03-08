70b25af026ca772f72a55d148b365444083fa27b
Alishka <141027048+alishka810@users.noreply.github.com>
2024-03-09 00:36:19 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -377,7 +377,7 @@ const Home = () => {
               );
 
             return (
-              <div styles={{"border": "none !important"}}>
+              <div style={{"border": "none !important", "outline": "none !important"}}>
               <Link to={`/?page=${a}`}>
                 <div>{a}</div>
               </Link>
