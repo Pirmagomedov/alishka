83b674b458aedf1e3a1f4f3c7be16c050c195835
Alishka <58191115+Pirmagomedov@users.noreply.github.com>
2024-12-08 22:38:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React6666666666
         </a>
       </header>
     </div>
