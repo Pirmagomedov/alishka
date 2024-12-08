1286d342f844bec232176595b7c63ddada1cceed
Pirmagomedov <gergotavra055@mail.ru>
2024-12-08 23:02:09 +0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  interface f {
+    f(): void
+  }
+
   return (
     <div className="App">
       <header className="App-header">
