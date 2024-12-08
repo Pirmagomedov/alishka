a0ded8acea2d65bd57dc9f64d3e401d436f0ba7b
Pirmagomedov <gergotavra055@mail.ru>
2024-12-08 22:48:19 +0300

    6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React6666666666
+          Learn React777
         </a>
       </header>
     </div>
