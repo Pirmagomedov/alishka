2670660675cd90993671fe538b3bea982ac9a175
Pirmagomedov <gergotavra055@mail.ru>
2024-12-08 23:52:22 +0300

    кк

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
 import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 function App() {
 
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <h1>Добро пожаловать в Аль-Хайр!</h1> }/>
+          <Route path="/*" element={ <h1>404</h1> }/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
