e67cdfbaae9dfbb2da9333afad975f0f0dadfe95
Pirmagomedov <gergotavra055@mail.ru>
2024-12-08 23:47:24 +0300

    t

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
 function App() {
 
-  interface f {
-    f(): void
-  }
-
-  class A implements f {
-    f() {
-      alert("Alimirza")
-    }
-  }
-
-  const alimirza: A = new A()
-
   return (
-    <div className="App" onClick={alimirza.f}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React7
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
