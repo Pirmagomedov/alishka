bae73ff371f3cc437f82efa41bf2fe273ff2ee67
Pirmagomedov <gergotavra055@mail.ru>
2024-12-09 13:14:37 +0300

    monday added

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <h1>Добро пожаловать в Аль-Хайр!</h1> }/>
+          <Route path="/" element={ <h1>Добро пожаловать в Аль-Хайр! Сегодня понедельник</h1> }/>
           <Route path="/*" element={ <h1>404</h1> }/>
         </Routes>
       </BrowserRouter>
